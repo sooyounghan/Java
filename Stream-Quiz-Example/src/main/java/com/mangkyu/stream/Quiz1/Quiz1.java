8bc6c043ace976840483ab7a9ae8a0fd8e93c3b7
sooyounghan <young940211@naver.com>
2024-06-17 05:57:55 +0900

    chore: Stream 문제 풀이 포함

---
+++ Stream-Quiz-Example/src/main/java/com/mangkyu/stream/Quiz1/Quiz1.java
@@ -0,0 +1,61 @@
+package com.mangkyu.stream.Quiz1;
+
+import com.opencsv.CSVReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Quiz1 {
+
+    private static final String TARGET = "좋아";
+    private static final int TARGET_LENGTH = TARGET.length();
+
+    // 1.1 각 취미를 선호하는 인원이 몇 명인지 계산하여라.
+    public Map<String, Integer> quiz1() throws IOException {
+        List<String[]> csvLines = readCsvLines();
+
+        return csvLines.stream()
+                .map(line -> line[1].replaceAll("\\s", ""))
+                .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> (newValue += oldValue)));
+    }
+
+    // 1.2 각 취미를 선호하는 정씨 성을 갖는 인원이 몇 명인지 계산하여라.
+    public Map<String, Integer> quiz2() throws IOException {
+        List<String[]> csvLines = readCsvLines();
+        return csvLines.stream()
+                .filter(line -> line[0].startsWith("정"))
+                .map(line -> line[1].replaceAll("\\s", ""))
+                .flatMap(hobbies -> Arrays.stream(hobbies.split(":")))
+                .collect(Collectors.toMap(hobby -> hobby, hobby -> 1, (oldValue, newValue) -> (newValue = ++oldValue)));
+    }
+
+    // 1.3 소개 내용에 '좋아'가 몇번 등장하는지 계산하여라.
+    public int quiz3() throws IOException {
+        List<String[]> csvLines = readCsvLines();
+        return csvLines.stream()
+                .map(line -> countContains(line[2], 0))
+                .reduce(0, Integer::sum);
+    }
+
+    private int countContains(String src, int fromIndex) {
+        int index = src.indexOf(TARGET, fromIndex);
+
+        if(index >= 0) {
+            return 1 + countContains(src, index + TARGET_LENGTH);
+        }
+        return 0;
+    }
+
+
+    private List<String[]> readCsvLines() throws IOException {
+        CSVReader csvReader = new CSVReader(new FileReader(getClass().getResource("/user.csv").getFile()));
+        csvReader.readNext();
+        return csvReader.readAll();
+    }
+
+}
